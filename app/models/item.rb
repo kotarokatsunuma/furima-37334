88a33686d28af26dbd5da6cd10adcd63a5423f9d
kotarokatsunuma <katsunuma1113@gmail.com>
2022-04-04 15:21:17 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
   belongs_to :user
   has_one_attached :image
+
+  validates :title, :text, presence: true
+  validates :concept, :text, presence: true
+
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank" } 
 end
